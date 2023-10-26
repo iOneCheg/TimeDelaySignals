07c31f97934223c33c1e1d6a5098f102417d0beb
TorunovIvan <torunov99@gmail.com>
2023-10-26 17:05:11 +0300

    Добавьте файлы проекта.

---
+++ GenerateSignals.cs
@@ -0,0 +1,293 @@
+﻿using DevExpress.XtraPrinting.Native;
+using MathNet.Numerics;
+using MathNet.Numerics.IntegralTransforms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace TimeDelaySignals
+{
+    public enum ModulationType
+    {
+        ASK,
+        FSK,
+        PSK
+    }
+
+    class GenerateSignals
+    {
+        /// <summary>
+        /// Тип модуляции
+        /// </summary>
+        private ModulationType Type { get; }
+        /// <summary>
+        /// Количество бит
+        /// </summary>
+        public int BitsCount { get; }
+        /// <summary>
+        /// Скорость передачи сигнала
+        /// </summary>
+        public int BaudRate { get; }
+        /// <summary>
+        /// Длительность одного бита
+        /// </summary>
+        public double BitLength => 1d / BaudRate;
+        /// <summary>
+        /// Длительность опорного сигнала
+        /// </summary>
+        public double Tsample => BitsCount / (double)BaudRate;
+        /// <summary>
+        /// Длительность исследуемого сигнала
+        /// </summary>
+        public double TBigSample => (double)(BitsCountBigPSP * 2 + BitsCount) / BaudRate;
+        /// <summary>
+        /// Несущая частота
+        /// </summary>
+        public int CarrierFreq { get; }
+        /// <summary>
+        /// Частота дискретизации
+        /// </summary>
+        public int SamplingFreq { get; }
+        /// <summary>
+        /// Шаг между отсчетами
+        /// </summary>
+        public double dt => 1d / SamplingFreq;
+        /// <summary>
+        /// Задержка второго сигнала
+        /// </summary>
+        public int Delay { get; }  // в мс
+        /// <summary>
+        /// Количество целых бит для исследуемого сигнала
+        /// </summary>
+        public int BitsCountBigPSP =>
+            (Delay % (int)(BitLength * 1000) != 0)
+            ? (Delay / (int)(BitLength * 1000) + 1)
+            : Delay / (int)(BitLength * 1000);
+        /// <summary>
+        /// Количество отсчетов на 1 бит
+        /// </summary>
+        public int TBit => (int)(TBigSample * SamplingFreq / (BitsCountBigPSP * 2 + BitsCount));
+        /// <summary>
+        /// Номер отсчета для отсечения "лишних" отсчетов не целого бита перед битом опорного сигнала
+        /// </summary>
+        private int CountStartCut => (int)(Delay / (dt * 1000));
+        /// <summary>
+        /// Номер отсчета начала опорного сигнала в исследуемом
+        /// </summary>
+        private int CountStart => BitsCountBigPSP * TBit;
+        /// <summary>
+        /// Номер отсчета конца опорного сигнала в исследуемом
+        /// </summary>
+        private int CountEnd => (BitsCountBigPSP + BitsCount) * TBit;
+        /// <summary>
+        ///  Номер отсчета для отсечения "лишних" отсчетов не целого бита после бита опорного сигнала
+        /// </summary>
+        private int CountEndCut => CountEnd + CountStartCut;
+        /// <summary>
+        /// Отсчеты исследумого сигнала
+        /// </summary>
+        public List<PointD> researchedSignal { get; private set; }
+        /// <summary>
+        /// Отсчеты опорного сигнала
+        /// </summary>
+        public List<PointD> desiredSignal { get; private set; }
+        /// <summary>
+        /// Отсчеты корреляции
+        /// </summary>
+        public List<PointD> correlation { get; private set; }
+
+        public GenerateSignals(Tuple<ModulationType, int, int, int, int, int> paramSignal)
+        {
+            researchedSignal = new List<PointD>();
+            desiredSignal = new List<PointD>();
+
+            Type = paramSignal.Item1;
+            BitsCount = paramSignal.Item2;
+            BaudRate = paramSignal.Item3;
+            CarrierFreq = paramSignal.Item4;
+            SamplingFreq = paramSignal.Item5;
+            Delay = paramSignal.Item6;
+        }
+        /// <summary>
+        /// Генерация ПСП для исследуемого сигнала
+        /// </summary>
+        /// <param name="rnd"></param>
+        /// <param name="CountBitsInDelay"></param>
+        /// <param name="BitsCount"></param>
+        /// <returns></returns>
+        public static int[] BigPSPGenerate(Random rnd, int CountBitsInDelay, int BitsCount) =>
+            Enumerable
+            .Range(0, CountBitsInDelay * 2 + BitsCount)
+            .Select(i => Convert.ToInt32(rnd.Next(2) == 0))
+            .ToArray();
+
+        public void ModulateSignals( Dictionary<string, object> modulateParam)
+        {
+            //Рассчет ПСП
+            int[] Bigpsp = BigPSPGenerate(new Random(Guid.NewGuid().GetHashCode()),
+              BitsCountBigPSP, BitsCount);
+
+            int counter = 0, counterAll = 0;
+
+            //Внешний цикл по длине ПСП
+            for (int bit = 0; bit < Bigpsp.Length; bit++)
+                //Цикл по каждому биту
+                for (int i = 0; i < TBit; i++)
+                {
+                    //Условие для "отрезания" отсчетов не целых бит при не кратной задержке
+                    if (!((counterAll > CountStartCut && counterAll < CountStart) || (counterAll > CountEndCut)))
+                    {
+                        double kAmplitude = (Bigpsp[bit] == 1d) ? (double)modulateParam["a1"] : (double)modulateParam["a0"],
+                               kPhase = (Bigpsp[bit] == 1d) ? Math.PI : 0,
+                               kFreq = (Bigpsp[bit] == 1d) ? (int)modulateParam["f1"] : (int)modulateParam["f0"];
+
+                        double v = Type switch
+                        {
+                            ModulationType.ASK =>
+                            kAmplitude * Math.Sin(2 * Math.PI * CarrierFreq * counter * dt),
+
+                            ModulationType.PSK =>
+                            Math.Sin(2 * Math.PI * CarrierFreq * counter * dt + Math.PI + kPhase),
+
+                            ModulationType.FSK =>
+                            Math.Sin(2 * Math.PI * kFreq * counter * dt),
+
+                            _ => 0
+                        };
+                        researchedSignal.Add(new PointD(counter * dt, v));
+
+                        //Условие для отсчетов опорного сигнала
+                        if (counter >= CountStart && counter < CountEnd)
+                            desiredSignal.Add(new PointD(counter * dt, v));
+
+                        counter++;
+                    }
+                    counterAll++;
+                }
+
+        }
+        /// <summary>
+        /// Наложение шума на сигналы
+        /// </summary>
+        /// <param name="snrDb">SNR в дБ</param>
+        public void MakeNoise(double snrDb)
+        {
+            // Наложение шума на искомый сигнал.
+            desiredSignal = desiredSignal.Zip(
+                    GenerateNoise(desiredSignal.Count, desiredSignal.Sum(p => p.Y * p.Y), 10),
+                    (p, n) => new PointD(p.X, p.Y + n))
+                .ToList();
+
+            // Наложение шума на исследуемый сигнал.
+            researchedSignal = researchedSignal.Zip(
+                    GenerateNoise(researchedSignal.Count, researchedSignal.Sum(p => p.Y * p.Y), snrDb),
+                    (p, n) => new PointD(p.X, p.Y + n))
+                .ToList();
+        }
+        /// <summary>
+        /// Рассчет корреляции
+        /// </summary>
+        /// <param name="maxIndex">Индекс max корреляции</param>
+        public void CalculateCorrelation(out int maxIndex)
+        {
+            var result = new List<PointD>();
+            var maxCorr = double.MinValue;
+            var index = 0;
+            for (var i = 0; i < researchedSignal.Count - desiredSignal.Count + 1; i++)
+            {
+                var corr = 0d;
+                for (var j = 0; j < desiredSignal.Count; j++)
+                    corr += researchedSignal[i + j].Y * desiredSignal[j].Y;
+                result.Add(new PointD(researchedSignal[i].X, corr / desiredSignal.Count));
+
+                if (result[i].Y > maxCorr)
+                {
+                    maxCorr = result[i].Y;
+                    index = i;
+                }
+            }
+
+            maxIndex = index;
+            correlation = result;
+        }
+        /// <summary>
+        /// Генерация случайного числа по нормальному распределению
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        private static double GetNormalRandom(double min, double max, int n = 12)
+        {
+            var rnd = new Random(Guid.NewGuid().GetHashCode());
+            var sum = 0d;
+            for (var i = 0; i < n; i++)
+                sum += rnd.NextDouble() * (max - min) + min;
+            return sum / n;
+        }
+        /// <summary>
+        /// Генерация белого гауссовского шума
+        /// </summary>
+        /// <param name="countNumbers">Число отсчетов</param>
+        /// <param name="energySignal">Энергия сигнала</param>
+        /// <param name="snrDb">SNR в дБ</param>
+        /// <returns></returns>
+        private static IEnumerable<double> GenerateNoise(int countNumbers, double energySignal, double snrDb)
+        {
+            var noise = new List<double>();
+            for (var i = 0; i < countNumbers; i++)
+                noise.Add(GetNormalRandom(-1d, 1d));
+
+            // Нормировка шума.
+            var snr = Math.Pow(10, -snrDb / 10);
+            var norm = Math.Sqrt(snr * energySignal / noise.Sum(y => y * y));
+
+            return noise.Select(y => y * norm).ToList();
+        }
+        public void CrossCorrelate(out int Max)
+        {
+
+            int N = Math.Max(researchedSignal.Count, desiredSignal.Count);
+            Complex32[] xComplex = new Complex32[N];
+            Complex32[] yComplex = new Complex32[N];
+
+            for (int i = 0; i < researchedSignal.Count; i++)
+            {
+                xComplex[i] = new Complex32((float)researchedSignal[i].Y, 0);
+            }
+
+            for (int i = 0; i < desiredSignal.Count; i++)
+            {
+                yComplex[i] = new Complex32((float)desiredSignal[i].Y, 0);
+            }
+
+            Fourier.Forward(xComplex);
+            Fourier.Forward(yComplex);
+
+            Complex[] product = new Complex[N];
+            for (int i = 0; i < N; i++)
+            {
+                product[i] = xComplex[i].ToComplex() * Complex.Conjugate(yComplex[i].ToComplex());
+            }
+
+            Fourier.Inverse(product);
+
+            List<PointD> result = new List<PointD>();
+            for (int i = 0; i < N; i++)
+            {
+                result.Add(new PointD(researchedSignal[i].X, product[i].Real));
+            }
+            double t = result.Max(p => p.Y);
+            Max = 0;
+            for (int i = 0; i < result.Count; i++)
+            {
+                if (result[i].Y == t) Max = i;
+
+            }
+            correlation = result;
+
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,273 @@
+﻿using DevExpress.XtraPrinting.Native;
+using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Drawing;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ScottPlot.Control;
+using DevExpress.Data.Linq.Helpers;
+using System.Collections;
+
+namespace TimeDelaySignals
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GenerateSignals _gS;
+        private ModulationType _modulationType;
+        private readonly BackgroundWorker _bgResearch;
+        private Dictionary<ModulationType, List<PointD>> _snrResearch;
+        private Tuple<ModulationType, int, int, int, int, int> signalParam;
+        private Dictionary<string,object> modParam;
+        private HashSet<object> researchParam;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _bgResearch = (BackgroundWorker)FindResource("BackgroundWorkerConductResearch");
+        }
+        #region #Radio Button#
+        private void RbIsAsk_Checked(object sender, RoutedEventArgs e)
+        {
+            _modulationType = ModulationType.ASK;
+            GbAskParams.IsEnabled = true;
+            GbFskParams.IsEnabled = false;
+        }
+
+        private void RbIsFsk_Checked(object sender, RoutedEventArgs e)
+        {
+            _modulationType = ModulationType.FSK;
+            GbAskParams.IsEnabled = false;
+            GbFskParams.IsEnabled = true;
+        }
+        private void RbIsPsk_Checked(object sender, RoutedEventArgs e)
+        {
+            _modulationType = ModulationType.PSK;
+            GbAskParams.IsEnabled = false;
+            GbFskParams.IsEnabled = false;
+        }
+        #endregion
+        #region #Buttons Methods#
+        private void GenerateSignals_Click(object sender, RoutedEventArgs e)
+        {
+            var countBits = BitsCount.Value;
+            var baudRate = BaudRate.Value;
+            var carrierFreq = double.Round((double)CarrierFreq.Value, 1) * 1000;
+            var samplingFreq = double.Round((double)SamplingFreq.Value, 1) * 1000;
+            var delay = Delay.Value;
+            var snr = SNR.Value;
+
+            var a0 = A0.Value;
+            var a1 = A1.Value;
+            var f1 = F1.Value;
+            var f0 = F0.Value;
+
+            int MaxIndex = 0;
+            //Параметры сигнала для модуляции
+            signalParam = new
+                (_modulationType, (int)countBits, (int)baudRate, (int)carrierFreq, (int)samplingFreq, (int)delay);
+
+            //Параметры модуляции
+            modParam = new Dictionary<string, object>
+            {
+                ["a0"] = a0,
+                ["a1"] = a1,
+                ["f0"] = f0,
+                ["f1"] = f1
+            };
+
+            //Рассчет модуляции, затем корреляции и нахождение max
+            _gS = new GenerateSignals(signalParam);
+            _gS.ModulateSignals(modParam);
+            _gS.MakeNoise((double)snr);
+            _gS.CrossCorrelate(out MaxIndex);
+            //_gS.CalculateCorrelation(out MaxIndex);
+
+            var yMax = _gS.desiredSignal.Max(p => double.Abs(p.Y));
+
+            ChartReferenceSignal.Plot.Clear();
+            ChartReferenceSignal.Plot.AddSignalXY(_gS.desiredSignal.Select(p => p.X).ToArray(),
+                _gS.desiredSignal.Select(p => p.Y).ToArray(), color: Color.Blue);
+            ChartReferenceSignal.Refresh();
+
+            ChartResearchedSignal.Plot.Clear();
+            ChartResearchedSignal.Plot.AddSignalXY(_gS.researchedSignal.Select(p => p.X).ToArray(),
+                _gS.researchedSignal.Select(p => p.Y).ToArray(), color: Color.Blue);
+            ChartResearchedSignal.Plot.AddVerticalLine((double)delay/1000d, Color.Green);
+            ChartResearchedSignal.Plot.AddVerticalLine((double)delay / 1000d + _gS.Tsample, Color.Green);
+            ChartResearchedSignal.Refresh();
+
+            ChartCorellation.Plot.Clear();
+            ChartCorellation.Plot.AddSignalXY(_gS.correlation.Select(p => p.X).ToArray(),
+                _gS.correlation.Select(p => p.Y).ToArray(), color: System.Drawing.Color.Blue);
+            ChartCorellation.Plot.AddVerticalLine((double)delay/1000d, Color.Green);
+            ChartCorellation.Plot.AddVerticalLine(MaxIndex * _gS.dt, Color.Red);
+            ChartCorellation.Refresh();
+
+            CorrelationDelay.Text = ((MaxIndex * _gS.dt)).ToString();
+        }
+
+        private void ConductResearch_Click(object sender, RoutedEventArgs e)
+        {
+            ConductResearch.Visibility = Visibility.Collapsed;
+            ProgressResearch.Visibility = Visibility.Visible;
+
+            _snrResearch = new Dictionary<ModulationType, List<PointD>>
+            {
+                [ModulationType.ASK] = new(),
+                [ModulationType.FSK] = new(),
+                [ModulationType.PSK] = new(),
+            };
+            var a0 = A0.Value;
+            var a1 = A1.Value;
+            var f1 = F1.Value;
+            var f0 = F0.Value;
+
+            var countBits = BitsCount.Value;
+            var baudRate = BaudRate.Value;
+            var carrierFreq = double.Round((double)CarrierFreq.Value, 1) * 1000;
+            var samplingFreq = double.Round((double)SamplingFreq.Value, 1) * 1000;
+            var delay = Delay.Value;
+
+            //Параметры сигнала для модуляции
+            signalParam = new
+                (_modulationType, (int)countBits, (int)baudRate, (int)carrierFreq, (int)samplingFreq, (int)delay);
+
+            //Параметры модуляции
+            modParam = new Dictionary<string, object>
+            {
+                ["a0"] = a0,
+                ["a1"] = a1,
+                ["f0"] = f0,
+                ["f1"] = f1
+            };
+            //Параметры исследования
+            researchParam = new HashSet<object>
+            { (int)MeanCount.Value, (int)UpBorder.Value, (int)DownBorder.Value, (double)Step.Value };
+
+            ProgressResearch.Value = 0;
+            ProgressResearch.Maximum = 3 * (int)researchParam.ElementAt(0) * ((int)researchParam.ElementAt(1) - (int)researchParam.ElementAt(2)) / ((double)researchParam.ElementAt(3) + 1);
+
+            _bgResearch.RunWorkerAsync();
+        }
+        #endregion
+        #region #BackgroundWorker Methods#
+        private void OnDoWorkBackgroundWorkerConductResearch(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            try
+            {
+                var meanOrder = (int)researchParam.ElementAt(0);
+                var snrFrom = (int)researchParam.ElementAt(2);
+                var snrTo = (int)researchParam.ElementAt(1);
+                var snrStep = (double)researchParam.ElementAt(3);
+
+                var index = 0;
+
+                Parallel.For(0, 3, type =>
+                {
+                    Parallel.For(0, (int)((snrTo - snrFrom) / snrStep + 1), n =>
+                    {
+                        var p = 0;
+                        var snr = snrFrom + n * snrStep;
+                        Parallel.For(0, meanOrder, i =>
+                        {
+                            int MaxIndex = 0;
+
+                            var sge = new GenerateSignals(new((ModulationType)type, signalParam.Item2,
+                                signalParam.Item3, signalParam.Item4, signalParam.Item5, signalParam.Item6));
+                            sge.ModulateSignals(modParam);
+                            sge.MakeNoise(snr);
+                            sge.CrossCorrelate(out MaxIndex);
+
+                            //Условие попадания найденного max в доверительный интервал
+                            if (MaxIndex <= (double)sge.Delay / (double)1000 / sge.dt + sge.TBit / 2 &&
+                                    MaxIndex >= (double)sge.Delay / (double)1000 / sge.dt - sge.TBit / 2)
+                                p++;
+
+                            _bgResearch.ReportProgress(++index);
+                        });
+                        _snrResearch[(ModulationType)type].Add(new PointD(snr, (double)p / meanOrder));
+                    });
+                    _snrResearch[(ModulationType)type] = _snrResearch[(ModulationType)type].OrderBy(p => p.X).ToList();
+                });
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Ошибка!", exception.Message);
+            }
+        }
+
+        private void OnRunWorkerCompletedBackgroundWorkerConductResearch(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            ConductResearch.Visibility = Visibility.Visible;
+            ProgressResearch.Visibility = Visibility.Collapsed;
+            ChartResearch.Visibility = Visibility.Visible;
+
+            ChartResearch.Plot.Clear();
+            ChartResearch.Plot.AddSignalXY(
+                  _snrResearch[ModulationType.ASK].Select(p => p.X).ToArray(),
+                _snrResearch[ModulationType.ASK].Select(p => p.Y).ToArray(),
+                Color.Red,
+                "ASK"
+            );
+            ChartResearch.Plot.AddSignalXY(
+                 _snrResearch[ModulationType.FSK].Select(p => p.X).ToArray(),
+                _snrResearch[ModulationType.FSK].Select(p => p.Y).ToArray(),
+                Color.Green,
+                "FSK"
+            );
+            ChartResearch.Plot.AddSignalXY(
+                 _snrResearch[ModulationType.PSK].Select(p => p.X).ToArray(),
+                _snrResearch[ModulationType.PSK].Select(p => p.Y).ToArray(),
+                Color.Blue,
+                "PSK"
+            );
+            ChartResearch.Plot.Legend();
+            ChartResearch.Plot.SetAxisLimits(xMin: (int)-20, xMax: (int)1, yMin: 0, yMax: 1);
+            ChartResearch.Refresh();
+        }
+
+        private void OnProgressChangedBackgroundWorkerConductResearch(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            ProgressResearch.Value = e.ProgressPercentage;
+        }
+        #endregion
+        private void OnLoadedMainWindow(object sender, RoutedEventArgs e)
+        {
+            RbIsAsk.IsChecked = true;
+
+            //Настройка графиков.
+            SetUpChart(ChartReferenceSignal, "Искомый сигнал", "Время, с", "Амплитуда");
+            SetUpChart(ChartResearchedSignal, "Исследуемый сигнал", "Время, с", "Амплитуда");
+            SetUpChart(ChartCorellation, "Взаимная корреляция сигналов", "Время, с", "Амплитуда");
+            SetUpChart(ChartResearch, "Зависимость вероятности обнаружения сигнала от ОСШ", "Уровень шума, дБ", "Вероятность обнаружения");
+        }
+        private static void SetUpChart(IPlotControl chart, string title, string labelX, string labelY)
+        {
+            chart.Plot.Title(title);
+            chart.Plot.XLabel(labelX);
+            chart.Plot.YLabel(labelY);
+            chart.Plot.XAxis.MajorGrid(enable: true, color: Color.FromArgb(50, Color.Black));
+            chart.Plot.YAxis.MajorGrid(enable: true, color: Color.FromArgb(50, Color.Black));
+            chart.Plot.XAxis.MinorGrid(enable: true, color: Color.FromArgb(30, Color.Black), lineStyle: LineStyle.Dot);
+            chart.Plot.YAxis.MinorGrid(enable: true, color: Color.FromArgb(30, Color.Black), lineStyle: LineStyle.Dot);
+            chart.Plot.Margins(x: 0.0, y: 0.8);
+            chart.Plot.SetAxisLimits(xMin: 0);
+            chart.Configuration.Quality = QualityMode.High;
+            chart.Configuration.DpiStretch = false;
+            chart.Refresh();
+        }
+    }
+}
